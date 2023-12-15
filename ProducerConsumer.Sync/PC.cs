75bb3b7025ed83a3af852647f2326e04b9538a8a
Marian Guceanu <marian.guceanu@yahoo.com>
2023-12-15 17:29:44 +0200

    done file server

--- ProducerConsumer.Sync/PC.cs
+++ ProducerConsumer.Sync/PC.cs
@@ -2,8 +2,6 @@ namespace ProducerConsumer.Sync
 {
     public static class PC
     {
-        // private static int[] VectorA = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; // Sample vector A
-        // private static int[] vectorB = { 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }; // Sample vector B
         private static int[] VectorA = { 1, 2, 3, 4, 5 }; // Sample vector A
         private static int[] vectorB = { 5, 4, 3, 2, 1 }; // Sample vector B
         private static int[] Products = new int[VectorA.Length]; // Array to store Products
@@ -26,10 +24,6 @@ namespace ProducerConsumer.Sync
 
         private static void Producer()
         {
-            // while (!ConsumerThread.IsAlive)
-            // {
-            //     Thread.Sleep(1000);
-            // }
             for (int i = 0; i < VectorA.Length; i++)
             {
                 lock (cond)
@@ -37,8 +31,6 @@ namespace ProducerConsumer.Sync
                     Console.WriteLine($"Producer: {i}");
                     Products[i] = VectorA[i] * vectorB[i];
                     Monitor.PulseAll(cond);
-                    // Thread.Sleep(1000);
-                    // Monitor.Wait(cond);
                 }
             }
             isProduced = true;
@@ -53,7 +45,6 @@ namespace ProducerConsumer.Sync
                     Monitor.PulseAll(cond);
                     Console.WriteLine($"Consumer: {i}");
                     sum += Products[i];
-                    // Thread.Sleep(1000);
                     Console.WriteLine($"Product of {VectorA[i]} and {vectorB[i]} is {Products[i]}");
                     Monitor.Wait(cond);
                 }
